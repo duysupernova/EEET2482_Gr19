345cc53402f987c42c73b4be53ef3ea033b8fac4
metalboxxx <minhkhang09122003@gmail.com>
2022-12-15 20:09:25 +0700

    isInPeriod finished

--- period.cpp
+++ period.cpp
@@ -8,12 +8,12 @@ class Period {
     tm startDate;
     tm endDate;
 public:
-    Period(tm startDateVal,tm EndDateVal){
+    Period(tm firstDateVal,tm secDateVal){
     /*
-    Format is dd/mm/yy
+    Automatically adjust which date is start and end
     */
-   this->startDate = startDateVal;
-   this->endDate = EndDateVal;
+   this->startDate = firstDateVal;
+   this->endDate = secDateVal;
 }
     void showInfo(){
         char* startInfo = new char[9];
@@ -21,11 +21,20 @@ public:
         char timeFormat[20] =  "%A %b%d %Y";
         strftime(startInfo,50,timeFormat,&startDate);
         strftime(endInfo,50,timeFormat,&endDate);
-        std::cout << "Period: " << startInfo << " --> " << endInfo;
+        std::cout << "Period: " << startInfo << " --> " << endInfo << "\n";
+    }
+    
+    bool isInPeriod(tm dateToCheck){                                        // Including start and date
+        return (difftime(mktime(&startDate),mktime(&dateToCheck)) < 0     // difftime return negative double if the first date is sooner
+                && difftime(mktime(&dateToCheck),mktime(&endDate)) < 0);   // Is the date between start and date 
+    }
+    bool isInPeriod(Period periodToCheck){                                                  // Focus on the false cases
+        return !(difftime(mktime(&periodToCheck.endDate),mktime(&this->startDate))<0        //  ___0--X____START--END_____
+            || difftime(mktime(&this->endDate),mktime(&periodToCheck.startDate))<0);        //  ___START--END____0--X____
     }
 };
 
-tm toTimeObj(int dd, int mm, int yy){
+tm toTM(int dd, int mm, int yy){
     time_t rawTime = time(NULL);             //Without raw time, localtime function doesnt work
     struct tm timeValue;                   
     timeValue = *localtime(&rawTime);        // Have to assign some value but can modify later
@@ -37,7 +46,32 @@ tm toTimeObj(int dd, int mm, int yy){
 }
 
 int main() {
-    Period period1(toTimeObj(11,11,2022),toTimeObj(22,11,2022));
+    Period period1(toTM(11,12,2022),toTM(16,12,2022));
+    Period period2(toTM(10,12,2022),toTM(14,12,2022));
     period1.showInfo();
+
+    time_t now = time(NULL);
+    struct tm nowlocal = *localtime(&now);
+
+    cout << period1.isInPeriod(nowlocal) << "\n";
+    cout << period2.isInPeriod(period1);
+
+/*    //Test the difftime function
+    time_t now; 
+    struct tm newyear; 
+    struct tm sometime;
+    double seconds;
+    time(&now);  // get current time; same as: now = time(NULL)  
+    newyear = *localtime(&now);
+    sometime = *localtime(&now);
+    newyear.tm_mday = 0;
+    newyear.tm_mon = 0;  
+    newyear.tm_year = 200;
+    sometime.tm_mday = 0;
+    sometime.tm_mon = 1;  
+    sometime.tm_year = 200;
+    seconds = difftime(mktime(&newyear),mktime(&sometime));             
+    printf ("%.f diffrence between 2 times\n", seconds);
+/**/
     return 0;
 }
